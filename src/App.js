e29429b7c5b182fee79b3f6c03808ea3d0e35645
xkostolanskym <kostolansky.marek@gmail.com>
2023-06-01 21:23:05 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import "./styles.css";
+import headerback from "./images/headerback.png";
+import headerimg from "./images/headerimg.png";
 
 function App() {
   return (
-    <div>
+    <div className="bg-secondary">
       <Navbar className="bg-primary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="#home" className="text-white">
+            kiddos
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#write">Write</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
+
       <Container>
-        <Row className="px-4 my-5">
+        {/* header */}
+
+        <Row className="px-4 my-5 container-md" img={headerback}>
           <Col sm={7}>
-            <Image src="https://picsum.photos/700/300" fluid rounded />
-          </Col>
-          <Col sm={5}>
-            <h1 className="font-weigh-light">Tagline</h1>
+            <h1 className="font-weigh-light ">
+              Dive Into The Success of Kid Learning
+            </h1>
             <p className="mt-4">
               As network-as-a-service gains traction, enterprisesmust consider
               their deployment models. See how SD-WAN, SASE, SDN and policy
@@ -51,6 +40,9 @@ function App() {
               Guides. Meet Our Editors.
             </p>
             <Button variant="outline-primary">Click me Now</Button>
+          </Col>
+          <Col sm={5}>
+            <Image src={headerimg} fluid rounded />
           </Col>
         </Row>
         <Row>
