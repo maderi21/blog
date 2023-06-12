03de7646a821d9b5e2f0b60cc67eaffe8119ec51
xkostolanskym <kostolansky.marek@gmail.com>
2023-06-12 23:17:29 +0200

    text field final

--- src/App.js
+++ src/App.js
@@ -71,50 +71,47 @@ function App() {
           </Row>
 
           {/* text field */}
-          <Row>
-            <Col sm={6} className="align-items-center">
-              <h1>
+          <Row className="py-5">
+            <Col sm={6}>
+              <h1 className="mt-4 mx-5 text-primary fs-2 fw-bold">
                 Education plays a vital role in shaping the future of our
-                children and society as a whole.{" "}
+                children and society as a whole.
               </h1>
             </Col>
-            <Col
-              sm={6}
-              className="d-flex justify-content-center align-items-center"
-            >
-              <p>
+            <Col sm={6} className="px-3">
+              <p className="mt-3 mx-5 mb-3">
                 Fostering Social and Emotional Development: Education is not
                 solely about academics; it also plays a crucial role in shaping
                 children's character and values
               </p>
-              <Col sm={3}>
-                <div className="d-flex justify-content-space-between px-5 mx-5">
-                  <FontAwesomeIcon
-                    className="text-primary"
-                    icon={faSquareCheck}
-                  />
-                  <p>Building Fundation</p>
-                  <FontAwesomeIcon
-                    className="text-primary"
-                    icon={faSquareCheck}
-                  />
-                  <p>Promoting Health</p>
+              <Row>
+                <div className="text-center">
+                  <div className="col px-5 d-flex text-center">
+                    <FontAwesomeIcon
+                      className="text-primary"
+                      icon={faSquareCheck}
+                    />
+                    <p className="px-2 mx-1">Building Fundation</p>
+                    <FontAwesomeIcon
+                      className="text-primary"
+                      icon={faSquareCheck}
+                    />
+                    <p className="px-2 mx-1">Promoting Health</p>
+                  </div>
+                  <div className="col px-5 d-flex text-center">
+                    <FontAwesomeIcon
+                      className="text-primary"
+                      icon={faSquareCheck}
+                    />
+                    <p className="px-2 mx-1">Nurturing thinking</p>
+                    <FontAwesomeIcon
+                      className="text-primary px-1"
+                      icon={faSquareCheck}
+                    />
+                    <p className="px-2 mx-1">Changing world</p>
+                  </div>
                 </div>
-              </Col>
-              <Col sm={3}>
-                <div className="d-flex justify-content-space-between px-5 mx-5">
-                  <FontAwesomeIcon
-                    className="text-primary"
-                    icon={faSquareCheck}
-                  />
-                  <p>Nurturing thinking</p>
-                  <FontAwesomeIcon
-                    className="text-primary"
-                    icon={faSquareCheck}
-                  />
-                  <p>Changing world</p>
-                </div>
-              </Col>
+              </Row>
             </Col>
           </Row>
           <Row>
