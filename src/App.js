715f270c9856541b14fc7234a96ac09c932b7386
xkostolanskym <kostolansky.marek@gmail.com>
2023-06-12 23:55:30 +0200

    recent blogs

--- src/App.js
+++ src/App.js
@@ -122,13 +122,15 @@ function App() {
 
           {/* Recent block posts */}
           <Row className="bg-danger">
-            <Col sm={8}>
-              <p>Recent blog posts</p>
-              <Card>
+            <Col sm={7} className="px-5">
+              <p className="text-primary fw-bold">Recent blog posts</p>
+              <Card className="border-0">
                 <Card.Img variant="top" src={blog1} />
-                <Card.Body>
-                  <Card.Text>Marek Kostolansky/20 Jan 2024</Card.Text>
-                  <Card.Title className="text-primary">
+                <Card.Body className="bg-danger">
+                  <Card.Text className="">
+                    Marek Kostolansky/20 Jan 2024
+                  </Card.Text>
+                  <Card.Title className="text-primary fw-bold">
                     Education is not solely about academics
                   </Card.Title>
                   <Card.Text>
@@ -137,47 +139,101 @@ function App() {
                     interact with peers, collaborate, empathize, and resolve
                     conflicts.
                   </Card.Text>
-                  <Card.Link>Read more</Card.Link>
-                  <FontAwesomeIcon icon={faArrowRight} />
+                  <Card.Link className="text-decoration-none">
+                    Read more
+                  </Card.Link>
+                  <FontAwesomeIcon className="px-2" icon={faArrowRight} />
                 </Card.Body>
               </Card>
             </Col>
-            <Col sm={4}>
-              <Card style={{ width: "10rem" }}>
-                <Card.Img variant="top" src={blog2} />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card style={{ width: "10rem" }}>
-                <Card.Img variant="top" src={blog3} />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-              <Card style={{ width: "10rem" }}>
-                <Card.Img variant="top" src={blog4} />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
+            <Col sm={5} className="mt-5">
+              <div className="card bg-danger border-0 mb-5">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={blog2} className="img-fluid" alt="blog1" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <small className="card-title text-muted">
+                        Marek Kostolansky/20 Jan 2024
+                      </small>
+                      <p className="card-text text-primary fw-bold">
+                        Education is not solely about academics
+                      </p>
+                      <Card.Link className="text-decoration-none">
+                        Read more
+                      </Card.Link>
+                      <FontAwesomeIcon className="px-2" icon={faArrowRight} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card bg-danger border-0 mb-5">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={blog2} className="img-fluid" alt="blog1" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <small className="card-title text-muted">
+                        Marek Kostolansky/20 Jan 2024
+                      </small>
+                      <p className="card-text text-primary fw-bold">
+                        Education is not solely about academics
+                      </p>
+                      <Card.Link className="text-decoration-none">
+                        Read more
+                      </Card.Link>
+                      <FontAwesomeIcon className="px-2" icon={faArrowRight} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card bg-danger border-0 mb-5">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={blog2} className="img-fluid" alt="blog1" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <small className="card-title text-muted">
+                        Marek Kostolansky/20 Jan 2024
+                      </small>
+                      <p className="card-text text-primary fw-bold">
+                        Education is not solely about academics
+                      </p>
+                      <Card.Link className="text-decoration-none">
+                        Read more
+                      </Card.Link>
+                      <FontAwesomeIcon
+                        className="px-2 cursor-pointer"
+                        icon={faArrowRight}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card bg-danger border-0 mb-5">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img src={blog2} className="img-fluid" alt="blog1" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <small className="card-title text-muted">
+                        Marek Kostolansky/20 Jan 2024
+                      </small>
+                      <p className="card-text text-primary fw-bold">
+                        Education is not solely about academics
+                      </p>
+                      <Card.Link className="text-decoration-none">
+                        Read more
+                      </Card.Link>
+                      <FontAwesomeIcon className="px-2" icon={faArrowRight} />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </Col>
           </Row>
 
