590de3010ad8dac022b6b72f369bb380a0cfd80c
xkostolanskym <kostolansky.marek@gmail.com>
2023-06-13 21:27:00 +0200

    newsletter final

--- src/App.js
+++ src/App.js
@@ -408,15 +408,24 @@ function App() {
               <img src={break2} className="img-fluid" />
             </Col>
           </Row>
-          <Row className="bg-danger">
+          <Row className="bg-danger py-5">
             <Col sm={6}>
-              <h3>Sign up to our NewsLetter</h3>
-              <p>
+              <h3 className="mx-5 mt-3">Sign up to our NewsLetter</h3>
+              <p className="mx-5">
                 Stay up tp date with the latest news, annoucements, and articles
               </p>
             </Col>
             <Col sm={6}>
-              <Form></Form>
+              <Form className="fs-5">
+                <div className="d-flex col-xs-4 justify-content-center p-4">
+                  <Form.Group className="mx-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter your email" />
+                  </Form.Group>
+                  <Button variant="btn btn-info rounded-pill text-white ">
+                    Join With Us
+                  </Button>
+                </div>
+              </Form>
             </Col>
           </Row>
         </Container>
