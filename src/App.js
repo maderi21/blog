da915ab847e6693b4350284e2d96fe3140583160
xkostolanskym <kostolansky.marek@gmail.com>
2023-07-21 21:03:48 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import styles from "./styles.css";
-import headerimg from "./images/headerimg.png";
 import break1 from "./images/break1.png";
 import break2 from "./images/break2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -16,39 +13,16 @@ import blog2 from "./images/blog2.jpg";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Header />
       <div className="bg-secondary">
         {/* header */}
         <Container>
-          <Row className="headerimg px-5 py-5 mt-5" style={styles}>
-            <Col sm={6} className="align-self-center">
-              <p className="fw-bold text-primary display-3">
-                Dive Into The Success of Kid Learning
-              </p>
-              <p className="mt-1 fw-light">
-                As network-as-a-service gains traction, enterprisesmust consider
-                their deployment models. See how SD-WAN, SASE, SDN and policy
-                management play a role in NaaS deployment. Watch Videos. See
-                Guides. Meet Our Editors.
-              </p>
-              <Button variant="btn btn-info rounded-pill text-white px-3 ">
-                Join With Us
-              </Button>
-            </Col>
-            <Col sm={6}>
-              <Image src={headerimg} className="container" />
-            </Col>
-          </Row>
-          <Row>
-            <Col className="px-0">
-              <img src={break1} className="img-fluid" />
-            </Col>
-          </Row>
-
           {/* text field */}
           <Row className="py-5">
             <Col sm={6}>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+import { Container, Col, Row, Button, Image } from "react-bootstrap";
+import styles from "../styles.css";
+import headerimg from "../images/headerimg.png";
+import break1 from "../images/break1.png";
 
 const Header = () => {
   return (
     <Container>
       <img src="./images/headerback.png" alt="Image" />
+      <Row className="headerimg px-5 py-5 mt-5" style={styles}>
+        <Col sm={6} className="align-self-center">
+          <p className="fw-bold text-primary display-3">
+            Dive Into The Success of Kid Learning
+          </p>
+          <p className="mt-1 fw-light">
+            As network-as-a-service gains traction, enterprisesmust consider
+            their deployment models. See how SD-WAN, SASE, SDN and policy
+            management play a role in NaaS deployment. Watch Videos. See Guides.
+            Meet Our Editors.
+          </p>
+          <Button variant="btn btn-info rounded-pill text-white px-3 ">
+            Join With Us
+          </Button>
+        </Col>
+        <Col sm={6}>
+          <Image src={headerimg} className="container" />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="px-0">
+          <img src={break1} className="img-fluid" />
+        </Col>
+      </Row>
     </Container>
   );
 };
